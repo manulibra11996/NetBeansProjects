d5bbdb52a157290cb3ce27992ff181ad07cb6b1b
manuellibra <86914767+manulibra11996@users.noreply.github.com>
2021-09-15 10:55:59 +0200

    descanso

--- com.arelance_sgaEJB_JPA_Relacional/src/main/java/com/arelance/sgajpa/domain/Persona.java
+++ com.arelance_sgaEJB_JPA_Relacional/src/main/java/com/arelance/sgajpa/domain/Persona.java
@@ -7,10 +7,8 @@ package com.arelance.sgajpa.domain;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
