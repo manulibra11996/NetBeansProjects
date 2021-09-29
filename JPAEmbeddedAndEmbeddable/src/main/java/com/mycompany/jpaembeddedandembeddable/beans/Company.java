a3538a554d7b6b8715d563ee5283533c3466ee45
manuellibra <86914767+manulibra11996@users.noreply.github.com>
2021-09-29 20:41:18 +0200

    Baeldung

---
+++ JPAEmbeddedAndEmbeddable/src/main/java/com/mycompany/jpaembeddedandembeddable/beans/Company.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jpaembeddedandembeddable.beans;
+
+/**
+ *
+ * @author manulibra
+ */
+public class Company {
+    
+    private Integer id;
+
+    private String name;
+
+    private String address;
+
+    private String phone;
+
+    private String contactFirstName;
+
+    private String contactLastName;
+
+    private String contactPhone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+    
+    
+}
