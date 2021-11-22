d554c618a08b3513b2947b931cd5778fa0905fff
DESKTOP-PMUSVT9\manuel <manulibra1996@gmail.com>
2021-11-22 09:51:17 +0100

    Prueba subido

---
+++ Pueba/Pueba/src/main/java/com/mycompany/pueba/Persona.java
@@ -0,0 +1,98 @@
+package com.mycompany.pueba;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author salvador
+ */
+@Named(value = "persona")
+@RequestScoped
+public class Persona {
+
+    private String nombre;
+    private String email;
+    private String sexo="Mujer";
+
+    private String[] marcaTelefoMovil;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Creates a new instance of Persona
+     */
+    public Persona() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+    /**
+     * @return the marcaTelefoMovil
+     */
+    
+    public String[] getMarcaTelefoMovil() {
+        return marcaTelefoMovil;
+    }
+
+    /**
+     * @param marcaTelefoMovil the marcaTelefoMovil to set
+     */
+    public void setMarcaTelefoMovil(String[] marcaTelefoMovil) {
+        this.marcaTelefoMovil = marcaTelefoMovil;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public Sexo[] getValues() {
+        return Sexo.values();
+    }
+    public enum Sexo {
+    HOMBRE("hombre"),MUJER("Mujer");
+
+    private Sexo(String descripcion) {
+        this.descripcion = descripcion;
+    }
+     
+    private String descripcion;
+   
+    
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+}
+
+}
