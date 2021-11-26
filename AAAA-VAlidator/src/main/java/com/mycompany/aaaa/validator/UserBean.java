5af6f6e5d115cc67158af801ae91d5201744730c
DESKTOP-PMUSVT9\manuel <manulibra1996@gmail.com>
2021-11-26 12:00:29 +0100

    hecho en clase

---
+++ AAAA-VAlidator/src/main/java/com/mycompany/aaaa/validator/UserBean.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aaaa.validator;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author manuel
+ */
+@Named(value = "user")
+@SessionScoped
+public class UserBean implements Serializable{
+  
+  String username;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+}
