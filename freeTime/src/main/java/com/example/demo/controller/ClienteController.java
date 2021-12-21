a6a29a6cd5f05d174191b699ce0b578cf8be3d16
DESKTOP-PMUSVT9\manuel <manulibra1996@gmail.com>
2021-12-21 13:34:53 +0100

    clase

--- freeTime/src/main/java/com/example/demo/controller/ClienteController.java
+++ freeTime/src/main/java/com/example/demo/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Cliente;
@@ -47,10 +49,30 @@ public class ClienteController {
 		
 		clienteRepository.save(cliente);
 		System.out.println("cliente guardado bien bien guay");
-		return "redirect:listausers";
+		return "redirect:users";
 		
 	}
 	
+	
+	@GetMapping("/edit/{id}")
+	public String editar(@PathVariable("id") Long idCliente, Model model) {
+		Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+		
+		
+		model.addAttribute("titulo","Formulario: Editar cliente");
+		model.addAttribute("cliente",cliente);
+		return "crearCliente";
+	}
+	
+	
+	@GetMapping("/delete/{id}")
+	public String eliminar(@PathVariable("id") Long idCliente) {
+
+		clienteRepository.deleteById(idCliente);
+		System.out.println("registro eliminado bien bueno");
+		
+		return "redirect:/users";
+	}
 	
 	@InitBinder	//yyyy-MM-dd USAR ESTE FORMATO PARA FECHA, EJEMPLO 1232-11-12
 	protected void initBinder(WebDataBinder binder) {
